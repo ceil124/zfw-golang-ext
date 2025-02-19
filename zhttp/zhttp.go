0a6ecc766073c01c95440fe8b855cf3f111a030a
ceil124 <ceil_124@163.com>
2025-02-19 14:29:42 +0800

    feat: 增加patch工具

--- zhttp/zhttp.go
+++ zhttp/zhttp.go
@@ -42,6 +42,20 @@ func HttpPostBody(url string, httpConfig HttpConfig) ([]byte, error) {
 	return body, nil
 }
 
+func HttpPatchBody(url string, httpConfig HttpConfig) ([]byte, error) {
+	res, err := HttpPatch(url, httpConfig)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
 // HttpGet 发起Get请求
 func HttpGet(url string, httpConfig HttpConfig) (*http.Response, error) {
 	method := "GET"
@@ -86,6 +100,47 @@ func HttpGet(url string, httpConfig HttpConfig) (*http.Response, error) {
 // HttpPost 发起Post请求
 func HttpPost(url string, httpConfig HttpConfig) (*http.Response, error) {
 	method := "POST"
+
+	client := &http.Client{}
+
+	// 设置 CheckRedirect 回调，禁止自动重定向
+	if httpConfig.Redirect {
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			// 返回 http.ErrUseLastResponse 表示停止重定向并返回最后的响应
+			return http.ErrUseLastResponse
+		}
+	}
+
+	payload := httpConfig.Payload
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := httpConfig.Headers
+	if headers != nil {
+		for key, val := range headers {
+			req.Header.Add(key, val)
+		}
+	}
+
+	cookies := httpConfig.Cookies
+	if cookies != nil {
+		for key, val := range cookies {
+			req.AddCookie(&http.Cookie{Name: key, Value: val})
+		}
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// HttpPatch 发起Patch请求
+func HttpPatch(url string, httpConfig HttpConfig) (*http.Response, error) {
+	method := "PATCH"
 
 	client := &http.Client{}
 
