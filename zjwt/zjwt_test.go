8717f55df8865034938d70dafa33173615db0e57
ceil124 <ceil_124@163.com>
2025-02-21 17:42:58 +0800

    feat: 调整单元测试

--- zjwt/zjwt_test.go
+++ zjwt/zjwt_test.go
@@ -1,20 +1,26 @@
 package zjwt
 
 import (
+	_ "embed"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"testing"
 )
 
+//go:embed test_cert/private_key.pem
+var privateKeyBytes []byte
+
+//go:embed test_cert/public_key.pem
+var publicKeyBytes []byte
+
 func TestJwtGenerateAndValidate(t *testing.T) {
 	// 引入testify断言库，简化单元测试
 	a := assert.New(t)
 
-	token, err := GenerateToken(1)
+	token, err := GenerateToken(privateKeyBytes, 1)
 	a.Nil(err)
 	log.Println(token)
 
-	err = ValidateToken(token)
+	err = ValidateToken(publicKeyBytes, token)
 	a.Nil(err)
-
 }
